da45421eab87ee43550d1a1f10aea281095ef87d
LOL <LOL2>
2019-12-23 14:48:59 +0530

    Working on enemy AI and navigation options

---
+++ Supercool_FPS/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    GameObject player;
+    float fireRate=0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Animator>().enabled == true)
+            //this.gameObject.transform.forward = (player.transform.position - this.gameObject.transform.position);
+            this.gameObject.transform.LookAt(player.transform);
+    }
+}
